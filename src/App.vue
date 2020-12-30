95a70bbcf76d3c1a67f1e83335934f85799c13a4
larissa <larissafurtado12@live.com>
2020-12-30 14:45:35 -0300

    create app and add navbar and hero components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+   <Navbar></Navbar>
+   <router-view></router-view>
   </div>
 </template>
-
+<script>
+import Navbar from './components/Navbar'
+export default {
+  components: { Navbar}
+}
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Nunito', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <nav class="navbar custom-nav fixed-top navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+          <router-link to="/" class="navbar-brand">Vue Shop</router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <router-link to="/" class="nav-link">Home</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/about" class="nav-link" href="#">About</router-link>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0">Get Start</button>
+            </form>
+          </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+@media (min-width: 992px) { 
+    
+    .navbar.custom-nav{
+      top:16px;
+      background-color: transparent !important;
+    }
+   }
+</style>
