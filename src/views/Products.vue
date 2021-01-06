e771a4291f81b931e53cfa2658bb836a6bfb51da
larissa <larissafurtado12@live.com>
2021-01-06 14:53:46 -0300

    watcher method added for realtime data update

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -114,10 +114,19 @@ export default {
     }
   },
   methods:{
+    watcher(){
+        db.collection("products").onSnapshot((querySnapshot) => {
+              this.products = [];
+              querySnapshot.forEach((doc) => {
+                  this.products.push(doc);
+              });
+          });
+    },
     updateProduct(){
       db.collection("products").doc(this.activeItem).update(this.product)
-        .then(function() {
+        .then(() => {
           $('#edit').modal('hide');
+          this.watcher();
             console.log("Document successfully updated!");
         })
         .catch(function(error) {
